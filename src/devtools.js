3755813cd0e5e52aa3018571c909fca1dd0d41d7
Kasper Byrdal Nielsen <byrdal76@gmail.com>
2017-10-06 18:24:04 +0200

    PoC

---
+++ src/devtools.js
@@ -0,0 +1,6 @@
+chrome.devtools.panels.create(
+    "HeaderGrep",
+    null,
+    "../html/panel.html",
+    null
+);
---
+++ src/panel.js
@@ -0,0 +1,16 @@
+chrome.devtools.network.onNavigated.addListener(
+    function(details) {
+        document.querySelector('#events').innerHTML = '';
+    }
+);
+
+chrome.devtools.network.onRequestFinished.addListener(
+    function(request) {
+        console.log(request.response.headers);
+        for (var i=0, len=request.response.headers.length; i < len; i++) {
+            var node = document.createElement("div");
+            node.innerHTML = request.response.headers[i].name + ': ' + request.response.headers[i].value;
+            document.querySelector('#events').appendChild(node);
+        }
+    }
+);
