8dcc77322a770e2478f736f8f08fbf585e720330
Kasper Byrdal Nielsen <byrdal76@gmail.com>
2017-10-13 17:05:24 +0200

    Add count filter function

--- src/panel.js
+++ src/panel.js
@@ -2,5 +2,11 @@ var Monkberry = require('monkberry'),
     Panel     = require('../view/panel.monk'),
     Listener  = require('../src/listener.js');
 
-const view = Monkberry.render(Panel, document.body);
+const filters = {
+    count: function(array) {
+        return array.length;
+    }
+};
+
+const view = Monkberry.render(Panel, document.body, {filters: filters});
 const listener = new Listener(view);
