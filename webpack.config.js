8b38032729fbe14682686f7ca27fb8fa7e269170
Kasper Byrdal Nielsen <byrdal76@gmail.com>
2020-09-04 09:38:13 +0200

    Upgrade to webpack 4 Fix dependency vulnerabilities

--- webpack.config.js
+++ webpack.config.js
@@ -1,50 +1,43 @@
 const path = require('path'),
-      ExtractTextPlugin = require("extract-text-webpack-plugin");
+      MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-const extractSass = new ExtractTextPlugin({
-    filename: "[name].css",
-    disable: process.env.NODE_ENV === "development"
-});
-
-module.exports =[
+module.exports = [
     {
+        mode: 'production',
         entry: [
-            './src/panel.js'
+            './src/panel.js',
+            './scss/styles.scss'
         ],
         output: {
-            filename: 'bundle.js',
-            path: path.resolve(__dirname, 'dist/js')
+            filename: 'js/bundle.js',
+            path: path.resolve(__dirname, 'dist/')
         },
         module: {
-            loaders: [
-                { test: /\.monk$/, loader: 'monkberry-loader' }
+            rules: [
+                {
+                    test: /\.monk$/,
+                    loader: 'monkberry-loader'
+                },
+                {
+                    test: /\.scss$/,
+                    use: [
+                        'style-loader',
+                        MiniCssExtractPlugin.loader,
+                        {
+                            loader: "css-loader",
+                        },
+                        {
+                            loader: "sass-loader"
+                        }
+                    ]
+                }
             ]
-        }
-    },
-    {
-        entry: {
-            styles: './scss/styles.scss'
-        },
-        output: {
-            filename: 'styles.css',
-            path: path.resolve(__dirname, 'dist/css')
-        },
-        module: {
-            rules: [{
-                test: /\.scss$/,
-                use: extractSass.extract({
-                    use: [{
-                        loader: "css-loader"
-                    }, {
-                        loader: "sass-loader"
-                    }],
-                    // use style-loader in development
-                    fallback: "style-loader"
-                })
-            }]
         },
         plugins: [
-            extractSass
+            new MiniCssExtractPlugin({
+                filename: "css/styles.css",
+                disable: process.env.NODE_ENV === "development"
+            }),
         ]
-    }
+    },
 ];
